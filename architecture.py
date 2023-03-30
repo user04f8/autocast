4cdf96b439b96943ef846c5b2e5529c4022e55ed
Nathan Clark <nc8774@gmail.com>
2023-03-30 17:05:48 -0400

    adding initial outline for architecture and tokenizer

---
+++ architecture.py
@@ -0,0 +1,31 @@
+import torch
+import torch.nn as nn
+from transformers import AutoModel, AutoTokenizer
+from tokenizer import preprocess_and_encode
+
+input_sentence = 'Hello world'
+
+class TransformerBinaryClassifier(nn.Module):
+    def __init__(self, model_name):
+        super(TransformerBinaryClassifier, self).__init__()
+        self.transformer = AutoModel.from_pretrained(model_name)
+        self.output = nn.Linear(self.transformer.config.hidden_size, 2)
+
+    def forward(self, input_ids, attention_mask):
+        outputs = self.transformer(input_ids=input_ids, attention_mask=attention_mask)
+        pooled_output = outputs[0][:, 0]  # Get the [CLS] token representation
+        logits = self.output(pooled_output)
+        return nn.functional.softmax(logits, dim=1)
+
+# Example usage
+model_name = "distilbert-base-uncased"  # Use any other model you prefer
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = TransformerBinaryClassifier(model_name)
+
+# Use the input_sentence from the previous example
+inputs = tokenizer(preprocess_and_encode(input_sentence), return_tensors="pt")
+input_ids, attention_mask = inputs["input_ids"], inputs["attention_mask"]
+
+# Get the prediction
+prediction = model(input_ids, attention_mask)
+print(prediction)
---
+++ tokenizer.py
@@ -0,0 +1,31 @@
+import nltk
+import numpy as np
+from gensim.models import KeyedVectors
+
+# Download the NLTK tokenizer data
+nltk.download('punkt')
+
+# Load pre-trained Word2Vec embeddings
+# Replace 'path/to/word2vec/model' with the actual path to the pre-trained Word2Vec model file
+word2vec_model = KeyedVectors.load_word2vec_format('path/to/word2vec/model', binary=True)
+
+def preprocess_and_encode(sentence):
+    # Tokenize the input sentence
+    tokens = nltk.word_tokenize(sentence.lower())
+    
+    # Encode each token using the Word2Vec model
+    encoded_tokens = []
+    for token in tokens:
+        if token in word2vec_model:
+            encoded_token = word2vec_model[token]
+            encoded_tokens.append(encoded_token)
+        else:
+            print(f"Warning: '{token}' not in the vocabulary. Skipping.")
+    
+    return np.array(encoded_tokens)
+
+if __name__ == '__main__':
+    # Example usage
+    input_sentence = "Will there be an initial public offering on either the Shanghai Stock Exchange or the Shenzhen Stock Exchange before 1 January 2016?"
+    encoded_sentence = preprocess_and_encode(input_sentence)
+    print(encoded_sentence)
